a8b17b0accb7daa0c94d0183e711e39cb5a8082d
Lacho06 <lazarodavidrodriguezgarcia06@gmail.com>
2023-04-19 14:15:27 +0200

    Problema con vite resuelto

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Patrimonio Cultural</title>
-    @vite(['resources/js/app.js', 'resources/css/app.scss'])
+    @vite(['resources/js/app.js', 'resources/sass/app.scss'])
     <style>
         *{
             margin: 0;
